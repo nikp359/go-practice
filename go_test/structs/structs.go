590a53e52e58c21d838b0aa136b3c3bf0c4511ae
Nikita Popov <nik359@gmail.com>
2019-01-20 23:53:49 +0300

    add Area

--- go_test/structs/structs.go
+++ go_test/structs/structs.go
@@ -1,14 +1,28 @@
 package structs
 
+import "math"
+
 type Rectagle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Shape interface {
+	Area() float64
 }
 
 func Perimeter(r Rectagle) float64 {
 	return 2 * (r.Width + r.Height)
 }
 
-func Area(r Rectagle) float64 {
+func (r Rectagle) Area() float64 {
 	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
 }
--- go_test/structs/structs_test.go
+++ go_test/structs/structs_test.go
@@ -15,9 +15,16 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectagle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
 
-	rectagle := Rectagle{10.0, 10.0}
-	got := Area(rectagle)
-	want := 100.0
-	assert.Equal(t, got, want, "they should be equal")
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		assert.Equal(t, got, tt.want, "they should be equal")
+	}
 }
